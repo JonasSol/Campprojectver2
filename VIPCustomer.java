740a0a8a9dc794dde2e1fe78383304673c3723ef
JonasSol <97516654+JonasSol@users.noreply.github.com>
2022-04-26 13:14:25 +0200

    Add files via upload

--- VIPCustomer.java
+++ VIPCustomer.java
@@ -3,13 +3,14 @@ public class VIPCustomer extends Client{
 	int VIPCardNo;
 	
 	
-	public VIPCustomer(int VIPCardNO, int ID, String name, int creditCardNo) {
+	public VIPCustomer(int VIPCardNO, int ID, String name, int creditCardNo, int VIPCardNo) {
 		super(ID, name, creditCardNo);
 		
 		this.VIPCardNo = VIPCardNo;
 	}
 	
 	public void setVIPCardNo(int newVIPCardNo) {
+		@SuppressWarnings("unused")
 		int VIPCardNo = newVIPCardNo;
 	}
 }
