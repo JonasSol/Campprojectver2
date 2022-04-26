7e373103afe250ff79064df608ba44bb437cb5d8
JonasSol <97516654+JonasSol@users.noreply.github.com>
2022-04-26 13:13:40 +0200

    Add files via upload

--- Reservation.java
+++ Reservation.java
@@ -1,13 +1,14 @@
 
-public class Reservation {
+public class Reservation extends Accomodation {
 	protected int IDReservation;
 	protected int date;
 	// no days stay? - must be extended from Accomodation class?
 	
-	public Reservation(int IDReservation, int date) {
+	public Reservation(int noPeople, int availability, int noDaysStay, boolean hasBBQ, int IDReservation, int date) {
+		super(noPeople, availability, noDaysStay, hasBBQ);
 		IDReservation = this.IDReservation;
 		date = this.date;
-		// make super()
+		
 	}
 	
 	public void setIDReservation(int newIDReservation) {
@@ -20,6 +21,15 @@ public class Reservation {
 	public String toString() {
 		return "date: " + date + "\n" + "IDReservation: " + IDReservation;
 	}
+	
+
+	
+	
+	public boolean CabinHasBBQ() {
+		return hasBBQ;
+	}
+	
+	
 	
 	
 }
